6c5396033b6270ce85b706b3ffba87b8afa2cd7f
Yanuar Arifin <3326885+ynrfin@users.noreply.github.com>
2019-12-30 20:33:35 +0700

    create item & category model on inventory" clear

--- factoryboi/inventory/models.py
+++ factoryboi/inventory/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    category = models.ForeignKey( 
+        'Category', 
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True)
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
