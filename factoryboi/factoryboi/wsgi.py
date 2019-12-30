6c642c5d973adf0d587fbbed63d13a0e10819836
Yanuar Arifin <3326885+ynrfin@users.noreply.github.com>
2019-12-30 20:11:33 +0700

    create project factoryboi

---
+++ factoryboi/factoryboi/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for factoryboi project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'factoryboi.settings')
+
+application = get_wsgi_application()
